5cf27270ac24929cf477416cacb56bc04627ec20
Rollo <danielzwierzchowski754@gmail.com>
2023-05-05 12:14:30 +0200

    Implemented adding new category view

--- app/src/main/java/eu/mobile/application/collector/fragment/categoryEntry/CategoryEntryFragment.kt
+++ app/src/main/java/eu/mobile/application/collector/fragment/categoryEntry/CategoryEntryFragment.kt
@@ -1,8 +1,60 @@
 package eu.mobile.application.collector.fragment.categoryEntry
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
+import eu.mobile.application.collector.R
+import eu.mobile.application.collector.databinding.FragmentCategoryEntryBinding
+import eu.mobile.application.collector.databinding.FragmentCategoryListBinding
+import eu.mobile.application.collector.fragment.categoryList.CategoryListFragment
+import eu.mobile.application.collector.fragment.categoryList.CategoryListViewModel
+import java.util.logging.Logger
 
 @AndroidEntryPoint
 class CategoryEntryFragment : Fragment()  {
+    companion object {
+        fun newInstance() = CategoryEntryFragment()
+        val logger = Logger.getLogger(CategoryEntryFragment::class.simpleName)
+    }
+
+    private lateinit var viewBinding: FragmentCategoryEntryBinding
+    private val viewModel by viewModels<CategoryEntryViewModel>()
+    private lateinit var arrayAdapter: ArrayAdapter<*>
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val root = inflater.inflate(R.layout.fragment_category_entry, container, false)
+        viewBinding = FragmentCategoryEntryBinding.bind(root).apply {
+            viewModel = this@CategoryEntryFragment.viewModel
+        }
+        viewBinding.lifecycleOwner = this.viewLifecycleOwner
+
+        return viewBinding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.initialize()
+        setupObservers()
+    }
+
+    private fun setupObservers() {
+        viewModel.addedCategoryLiveData.observe(viewLifecycleOwner){
+            if(it){
+                goToListCategory()
+            }
+        }
+    }
+
+    private fun goToListCategory(){
+        if(!findNavController().popBackStack(R.id.categoryListFragmentDestination,false))
+            findNavController().navigate(R.id.categoryListFragmentDestination)
+    }
 }
--- app/src/main/java/eu/mobile/application/collector/fragment/categoryEntry/CategoryEntryViewModel.kt
+++ app/src/main/java/eu/mobile/application/collector/fragment/categoryEntry/CategoryEntryViewModel.kt
@@ -1,9 +1,44 @@
 package eu.mobile.application.collector.fragment.categoryEntry
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import eu.mobile.application.collector.event.ErrorHandler
+import eu.mobile.application.collector.event.Message
+import eu.mobile.application.collector.repository.CategoryRepository
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class CategoryEntryViewModel @Inject constructor(): ViewModel() {
+class CategoryEntryViewModel @Inject constructor(
+    private val repository: CategoryRepository
+): ViewModel() {
+    var categoryEntryNotifier: MutableLiveData<String> = MutableLiveData()
+    private val addedCategoryNotifier: MutableLiveData<Boolean> = MutableLiveData()
+    val addedCategoryLiveData: LiveData<Boolean> = addedCategoryNotifier
+
+    fun addCategory(){
+        if(categoryEntryNotifier.value.isNullOrEmpty()){
+            ErrorHandler.postMessageEvent(Message().apply { message = "Musisz wpisać nazwę kategorii" })
+            return
+        }
+        viewModelScope.launch {
+            repository.addCategory(categoryEntryNotifier.value!!)
+                .onSuccess {
+                    ErrorHandler.postMessageEvent(Message().apply {message = "Dodano kategorię"})
+                    addedCategoryNotifier.value = true
+
+                }
+                .onFailure {
+                    ErrorHandler.postErrorMessageEvent(it)
+                }
+        }
+
+    }
+
+    fun initialize(){
+
+    }
 }
